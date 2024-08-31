60fcbf19c70d439a7ad0b81bdfda89d8617e1c34
Felipe de Oliveira <161166657+felipeolv11@users.noreply.github.com>
2024-08-31 15:15:35 -0300

    Add files via upload

---
+++ recursion/exercises/exercicio03.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio03
+{
+    class Program
+    {
+        static int FatorialDuplo(int n)
+        {
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("O numero precisa ser impar!");
+                
+                return -1;
+            }
+
+            else
+            {
+                if (n == 1)
+                {
+                    return n;
+                }
+
+                else
+                {
+                    return n * FatorialDuplo(n - 2);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int resultado = FatorialDuplo(9);
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ recursion/exercises/exercicio09.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio09
+{
+    class Program
+    {
+        static void Inverter(int[] vet, int inicio, int fim)
+        {
+            if (inicio >= fim)
+            {
+                return;
+            }
+
+            else
+            {
+                int temp = vet[inicio];
+                vet[inicio] = vet[fim];
+                vet[fim] = temp;
+
+                Inverter(vet, inicio + 1, fim - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha a quantidade de casas do vetor: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            int[] vet = new int [qtd];
+
+            Random random = new Random();
+
+            for (int i = 0; i < qtd; i++)
+            {
+                vet[i] = random.Next(1, 10);
+            }
+
+            Console.WriteLine("Vetor:");
+
+            foreach (int i in vet)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Vetor invertido:");
+
+            Inverter(vet, 0, qtd - 1);
+
+            foreach (int i in vet)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
