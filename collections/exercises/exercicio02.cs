4173a76846c8882d63c8304f8db68b7e6953c9ad
Felipe de Oliveira <felipeeh1524@gmail.com>
2024-09-10 13:14:03 -0300

    commit for exercises 02, 03

---
+++ collections/exercises/exercicio02.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            bool maiorque3 = false;
+            
+            do
+            {
+                Console.WriteLine("Digite um numero N: ");
+                n = int.Parse(Console.ReadLine());
+
+                if (n >= 3)
+                {
+                    maiorque3 = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("O numero precisa ser maior ou igual a tres.");
+                }
+
+            } while (!maiorque3);
+
+            Stack<int> s = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                s.Push(Fibonacci(i));
+            }
+
+            foreach (int num in s)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadLine();
+        }
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
---
+++ collections/exercises/exercicio03.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> bolsas_ic = new Queue<int>();
+            Queue<int> bolsas_mestrado = new Queue<int>();
+
+            int aluno = 0;
+            int ex_aluno = 0;
+            int primeiro_fila = 0;
+
+            int opcao = 0;
+
+            while (opcao != 11)
+            {
+                Console.WriteLine("UNIVERSIDADE ACME");
+                Console.WriteLine("=================");
+                Console.WriteLine("1 - Inserir um aluno na fila de espera de bolsas de Iniciação Científica");
+                Console.WriteLine("2 - Inserir um aluno na fila de espera de bolsas de Mestrado");
+                Console.WriteLine("3 - Remover um aluno da fila de bolsas de Iniciação Científica");
+                Console.WriteLine("4 - Remover um aluno da fila de bolsas de Mestrado");
+                Console.WriteLine("5 - Mostrar fila de espera de bolsas de Iniciação Científica");
+                Console.WriteLine("6 - Mostrar fila de espera de bolsas de Mestrado");
+                Console.WriteLine("7 - Pesquisar aluno na fila de espera de bolsas de Iniciação Científica");
+                Console.WriteLine("8 - Pesquisar aluno na fila de espera de bolsas de Mestrado");
+                Console.WriteLine("9 - Mostrar qual o aluno que está no início da fila de espera de bolsas de Iniciação Científica");
+                Console.WriteLine("10 - Mostrar qual o aluno que está no início da fila de espera de bolsas de Mestrado");
+                Console.WriteLine("11- Encerrar o programa");
+                opcao = int.Parse(Console.ReadLine());
+
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o codigo do aluno: ");
+                        aluno = int.Parse(Console.ReadLine());
+
+                        InserirAlunoFila(ref bolsas_ic, aluno);
+
+                        Console.WriteLine("O aluno foi inserido na fila de espera de bolsas de Iniciação Científica.");
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Digite o codigo do aluno: ");
+                        aluno = int.Parse(Console.ReadLine());
+
+                        InserirAlunoFila(ref bolsas_mestrado, aluno);
+
+                        Console.WriteLine("O aluno foi inserido na fila de espera de bolsas de Mestrado.");
+
+                        break;
+
+                    case 3:
+                        aluno = RemoverAlunoFila(ref bolsas_ic);
+
+                        Console.WriteLine("O aluno {0} foi removido da fila de espera de bolsas de Iniciação Científica.", aluno);
+
+                        break;
+
+                    case 4:
+                        aluno = RemoverAlunoFila(ref bolsas_mestrado);
+
+                        Console.WriteLine("O aluno {0} foi removido da fila de espera de bolsas de Mestrado.", aluno);
+
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Fila de espera de bolsas de Iniciação Científica:");
+
+                        MostrarFila(ref bolsas_ic);
+
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Fila de espera de bolsas de Mestrado:");
+
+                        MostrarFila(ref bolsas_mestrado);
+
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Digite o codigo do aluno: ");
+                        aluno = int.Parse(Console.ReadLine());
+
+                        if (PesquisarAluno(ref bolsas_ic, aluno))
+                        {
+                            Console.WriteLine("O aluno {0} está na fila de Iniciação Científica.", aluno);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("O aluno {0} não está na fila de Mestrado.", aluno);
+                        }
+
+                        break;
+
+                    case 8:
+                        Console.WriteLine("Digite o codigo do aluno: ");
+                        aluno = int.Parse(Console.ReadLine());
+
+                        if (PesquisarAluno(ref bolsas_mestrado, aluno))
+                        {
+                            Console.WriteLine("O aluno {0} está na fila de Iniciação Científica.", aluno);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("O aluno {0} não está na fila de Mestrado.", aluno);
+                        }
+
+                        break;
+
+                    case 9:
+                        aluno = PrimeiroDaFila(ref bolsas_ic);
+
+                        Console.WriteLine("O aluno {0} é o primeiro da fila de Iniciação Científica.", aluno);
+
+                        break;
+
+                    case 10:
+                        aluno = PrimeiroDaFila(ref bolsas_mestrado);
+
+                        Console.WriteLine("O aluno {0} é o primeiro da fila de Mestrado.", aluno);
+
+                        break;
+
+                    case 11:
+                        Console.WriteLine("FIM!");
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void InserirAlunoFila(ref Queue<int> q, int aln)
+        {
+            q.Enqueue(aln);
+        }
+
+        static int RemoverAlunoFila(ref Queue<int> q)
+        {
+            return q.Dequeue();
+        }
+
+        static void MostrarFila(ref Queue<int> q)
+        {
+            foreach (int hash in q)
+            {
+                Console.WriteLine("- " + hash);
+            }
+        }
+
+        static bool PesquisarAluno(ref Queue<int> q, int aln)
+        {
+            return q.Contains(aln);
+        }
+
+        static int PrimeiroDaFila(ref Queue<int> q)
+        {
+            return q.Peek();
+        }
+    }
+}
