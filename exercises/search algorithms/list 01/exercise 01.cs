60ab9a90dafd3ced4eea282658406e8dda3fcaa2
Felipe de Oliveira <felipeeh1524@gmail.com>
2024-11-03 01:20:02 -0300

    commit for exercise 01 and adjustments

---
+++ exercises/search algorithms/list 01/exercise 01.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o tamanho do array: ");
+            int tam = int.Parse(Console.ReadLine());
+            
+            int[] arr = new int[tam];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1, 100);
+            }
+
+            Console.WriteLine("Digite o alvo que deseja procurar: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int index = BuscaSequencial(arr, x);
+
+            if (index != -1)
+                Console.WriteLine("O alvo foi encontrado na posição: {0}", index);
+            
+            else
+                Console.WriteLine("O alvo não se encontra no array.");
+
+            Console.ReadLine();
+        }
+
+        static int BuscaSequencial(int[] arr, int x)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                if(arr[i] == x)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
